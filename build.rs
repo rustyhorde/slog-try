cff39b930283530a37c6934c84d76b2ed1f1d132
Jason Ozias <jason.g.ozias@gmail.com>
2022-12-10 10:36:12 -0500

    fixed lints

--- build.rs
+++ build.rs
@@ -1,13 +1,14 @@
-fn main() {
+pub fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     nightly_lints();
     beta_lints();
     stable_lints();
+    msrv_lints();
 }
 
 #[rustversion::nightly]
 fn nightly_lints() {
-    println!("cargo:rustc-cfg=nightly_lints");
+    println!("cargo:rustc-cfg=nightly");
 }
 
 #[rustversion::not(nightly)]
@@ -15,7 +16,7 @@ fn nightly_lints() {}
 
 #[rustversion::beta]
 fn beta_lints() {
-    println!("cargo:rustc-cfg=beta_lints");
+    println!("cargo:rustc-cfg=beta");
 }
 
 #[rustversion::not(beta)]
@@ -23,8 +24,16 @@ fn beta_lints() {}
 
 #[rustversion::stable]
 fn stable_lints() {
-    println!("cargo:rustc-cfg=stable_lints");
+    println!("cargo:rustc-cfg=stable");
 }
 
 #[rustversion::not(stable)]
 fn stable_lints() {}
+
+#[rustversion::before(1.65)]
+fn msrv_lints() {}
+
+#[rustversion::since(1.65)]
+fn msrv_lints() {
+    println!("cargo:rustc-cfg=msrv");
+}
